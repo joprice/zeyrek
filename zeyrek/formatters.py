901956c622b868daadfd8958fd4cb026f9d93cca
Joseph Price <pricejosephd@gmail.com>
2025-03-30 10:24:29 -0400

    ignore simple adjectives

--- zeyrek/formatters.py
+++ zeyrek/formatters.py
@@ -46,14 +46,15 @@ class UDFormatter(Formatter, ABC):
         self.add_surface = add_surface
 
     def format_adj(self, analysis: SingleAnalysis) -> str:
-        #ids = [m.id_ for m in analysis.morphemes]
+        ids = [m[0].id_ for m in analysis.morphemes]
         case = ''
         number = ''
         person = ''
         npsor = ''
         psor = ''
-        #if 'Noun' not in ids:
-        #    return self.format(analysis)
+
+        if 'Noun' not in ids:
+            return ''
 
         for m in analysis.morphemes:
             morph = m[0]
